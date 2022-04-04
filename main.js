603298c423e5420db7caf8a81598b578aa81494d
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-04 23:41:10 +0100

    Add js function to the nav

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-let menu = document.querySelector(".menu");
-let hamburger = document.querySelector(".menu-toggle");
-let close = document.querySelector(".close");
-let open = document.querySelector(".open");
-let items = document.querySelectorAll(".menu-item");
+let menu = document.querySelector('.menu')
+let hamburger = document.querySelector('.menu-toggle')
+let close = document.querySelector('.close')
+let open = document.querySelector('.open')
+let items = document.querySelectorAll('.menu-item')
 
-
-function toggleMenu(){
-  if (menu.classList.contains("showMenu")){
-   menu.classList.remove("showMenu")
-   close.style.display = "none"
-   open.style.display = "block"
-  }else {
-   menu.classList.add("showMenu")
-   close.style.display = "block"
-   open.style.display = "none"
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu')
+    close.style.display = 'none'
+    open.style.display = 'block'
+  } else {
+    menu.classList.add('showMenu')
+    close.style.display = 'block'
+    open.style.display = 'none'
   }
 }
