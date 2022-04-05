3990de1affd4848c923de4d449a7ae1adfac85ee
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-05 15:13:34 +0100

    Add mobile menu fucntions

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ let hamburger = document.querySelector('.menu-toggle')
 let close = document.querySelector('.close')
 let open = document.querySelector('.open')
 let items = document.querySelectorAll('.menu-item')
+let showMenu = document.querySelector('.showMenu')
+let logo = document.querySelector(".name-info")
+
+// hamburger.addEventListener('click', () => {
+//   close.classList.toggle('active')
+//   menu.classList.toggle('active')
+// })
 
 function toggleMenu() {
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu')
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active')
     close.style.display = 'none'
     open.style.display = 'block'
+    logo.style.display = 'block'
   } else {
-    menu.classList.add('showMenu')
+    menu.classList.add('active')
     close.style.display = 'block'
+    logo.style.display = "none"
     open.style.display = 'none'
   }
 }
 
-hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener('click', toggleMenu)
