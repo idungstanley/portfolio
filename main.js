5fa18da31fe98d9a9a224bb9a02d2f83a95bf366
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-05 16:34:31 +0100

    Fix linters error

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-let menu = document.querySelector('.menu')
-let hamburger = document.querySelector('.menu-toggle')
-let close = document.querySelector('.close')
-let open = document.querySelector('.open')
-let items = document.querySelectorAll('.menu-item')
-let showMenu = document.querySelector('.showMenu')
-let logo = document.querySelector(".name-info")
-
-// hamburger.addEventListener('click', () => {
-//   close.classList.toggle('active')
-//   menu.classList.toggle('active')
-// })
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.menu-toggle');
+const close = document.querySelector('.close');
+const open = document.querySelector('.open');
+const items = document.querySelectorAll('.menu-item');
+const logo = document.querySelector('.name-info');
+const { body } = document;
 
 function toggleMenu() {
   if (menu.classList.contains('active')) {
-    menu.classList.remove('active')
-    close.style.display = 'none'
-    open.style.display = 'block'
-    logo.style.display = 'block'
+    menu.classList.remove('active');
+    close.style.display = 'none';
+    open.style.display = 'block';
+    logo.style.display = 'block';
+    body.classList.remove('noScroll');
   } else {
-    menu.classList.add('active')
-    close.style.display = 'block'
-    logo.style.display = "none"
-    open.style.display = 'none'
+    menu.classList.add('active');
+    close.style.display = 'block';
+    body.classList.add('noScroll');
+    logo.style.display = 'none';
+    open.style.display = 'none';
   }
 }
 
-hamburger.addEventListener('click', toggleMenu)
+hamburger.addEventListener('click', toggleMenu);
+items.forEach((item) => item.addEventListener('click', toggleMenu));
