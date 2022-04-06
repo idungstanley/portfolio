7c5c0e52916fa21362d3a34c926416de5e867e5e
sunday stanley Idung <sundaystanley56@gmail.com>
2022-04-06 17:14:26 +0100

    Add the popup styles

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const items = document.querySelectorAll('.menu-item')
 const logo = document.querySelector('.name-info')
 const { body } = document
 const cardContainer = document.querySelector('.card-container')
-const article = document.createElement('article')
-const heading3 = document.createElement('h3')
-const paragraph = document.createElement('p')
-const listContainer = document.createElement('ul')
-const listItem = document.createElement('li')
-const btn = document.createElement('button')
-const link = document.createElement('a')
+// const article = document.createElement('article')
+// const heading3 = document.createElement('h3')
+// const paragraph = document.createElement('p')
+// const listContainer = document.createElement('ul')
+// const listItem = document.createElement('li')
+// const btn = document.createElement('button')
+// const link = document.createElement('a')
 
 let project = [
   {
@@ -72,41 +72,50 @@ let project = [
 ]
 
 function loadCardData() {
+  for (let i = 0; i < project.length; i++) {
+    const article = document.createElement('article')
+    const heading3 = document.createElement('h3')
+    const paragraph = document.createElement('p')
+    const listContainer = document.createElement('ul')
+    const btn = document.createElement('button')
 
-for(let i = 0; i < project.length; i++){
-  const article = document.createElement('article')
-  const heading3 = document.createElement('h3')
-  const paragraph = document.createElement('p')
-  const listContainer = document.createElement('ul')
-  const btn = document.createElement('button')
+    let name = project[i].title
+    let description = project[i].description
+    let technologies = project[i].technologies
+    cardContainer.appendChild(article)
+    article.classList.add('bcg-container', 'project-card')
+    if (i > 0) {
+      article.classList.add('cd')
+    }
+    article.append(heading3, paragraph, listContainer, btn)
+    for (let j = 0; j < technologies.length; j++) {
+      const link = document.createElement('a')
+      const listItem = document.createElement('li')
+      listContainer.appendChild(listItem)
+      listContainer.classList.add('list-container')
+      listItem.appendChild(link)
+      listItem.classList.add('list-2')
+      link.innerText = technologies[j]
+    }
 
-  let name = project[i].title
-  let description = project[i].description
-  let technologies = project[i].technologies
-  cardContainer.appendChild(article)
-  article.classList.add('bcg-container', 'project-card')
-  if(i>0){
-    article.classList.add("cd")
-  }
-  article.append(heading3, paragraph, listContainer, btn)
-  for(let j = 0; j < technologies.length; j++){
-    const link = document.createElement('a')
-    const listItem = document.createElement('li')
-    listContainer.appendChild(listItem)
-    listContainer.classList.add('list-container')
-    listItem.appendChild(link)
-    listItem.classList.add('list-2')
-    link.innerText = technologies[j]
-  }
-
-  heading3.innerText = project[i].title
-  paragraph.innerText = description
-  btn.innerText = 'See Project'
-  btn.classList.add('base-btn')
-  }
+    heading3.innerText = project[i].title
+    paragraph.innerText = description
+    btn.innerText = 'See Project'
+    btn.classList.add('base-btn', "pop")
+    btn.setAttribute("id", i)
+}
 }
 
-loadCardData(project)
+
+
+const popbtn = document.querySelectorAll(".pop")
+popbtn.addEventListener("click", (event)=>{
+console.log(event.srcElement.id);
+})
+
+
+
+loadCardData()
 
 function toggleMenu() {
   if (menu.classList.contains('active')) {
@@ -124,5 +133,7 @@ function toggleMenu() {
   }
 }
 
-hamburger.addEventListener('click', toggleMenu)
-items.forEach((item) => item.addEventListener('click', toggleMenu))
+hamburger.addEventListener('click', toggleMenu())
+items.forEach((item) => {
+  item.addEventListener('click', toggleMenu)
+})
